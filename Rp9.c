f1542abcaf0d0066f71243c3e545a4c4ce15c1eb
Rutvisojitra <rutvisojitra28@gmail.com>
2024-08-05 15:59:50 +0530

    Add files via upload

---
+++ Rp9.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int m;
+     printf("enter the mark");
+     scanf("%d",&m);
+
+       m>=90?printf("grade A"):m>=80?printf("grade B"):m>=70?printf("grade C"):m>=60?printf("grade D"):printf("fail");
+
+
+
+
+}

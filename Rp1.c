66a102f6e9762657b202fb94ede9d150e3888530
Rutvisojitra <rutvisojitra28@gmail.com>
2024-08-09 16:00:52 +0530

    Add files via upload

---
+++ Rp1.c
@@ -0,0 +1,47 @@
+/*
+PROGRAMMER:RUTVI SOJITRA
+DATE:9 AUGUST 2024
+AIM:PRINT DIAMOND
+*/
+#include<stdio.h>
+void main()
+{
+    int r,c,n,n1,n2;
+    printf("enter the value of n");
+    scanf("%d",&n);
+    n1=n/2+1;
+    n2=n/2;
+    for(r=1;r<=n1;r++)
+    {
+        for(c=1;c<=n1-r;c++)
+        {
+            printf("  ");
+        }
+        for(c=n1-r+1;c<=n1;c++)\
+        {
+            printf(" *");
+        }
+        for(c=1;c<r-1;c++)
+        {
+            printf(" *");
+        }
+        printf("\n");
+    }
+    for(r=1;r<=n2;r++)
+    {
+        for(c=1;c<=r;c++)
+        {
+            printf("  ");
+        }
+
+        for(c=1;c<=n2-r+1;c++)
+        {
+            printf(" *");
+        }
+        printf("\n");
+        for(c=n2-r+1;c<=n2-r;c++)
+        {
+            printf(" *");
+        }
+    }
+}

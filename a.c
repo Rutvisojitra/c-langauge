21062608cb999674d0f1a28425ae34d716f863a7
Rutvisojitra <rutvisojitra28@gmail.com>
2025-02-07 00:15:07 +0530

    Add files via upload

---
+++ a.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int g,h,i;
+
+    printf("enter the value of g \n");
+scanf("%d",&g);
+
+printf("enter the value of h \n");
+scanf("%d",&h);
+
+printf("enter the value of i \n");
+scanf("%d",&i);
+
+printf("%d",g*h/i);
+}

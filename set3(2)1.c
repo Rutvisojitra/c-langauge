01a7f3960a7decfeeb0fb9fbd582f0314e5c5d33
Rutvisojitra <rutvisojitra28@gmail.com>
2025-02-07 00:18:11 +0530

    Add files via upload

---
+++ set3(2)1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+  
+int x,y,z;
+
+printf("enter the value of x \n");
+scanf("%d",&x);
+
+printf("enter the value of y \n");
+scanf("%d",&y);
+
+printf("enter the value of z \n");
+scanf("%d",&z);
+
+printf("%d",x+y*z);
+}

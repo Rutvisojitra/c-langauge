af7a1f54e0812ebea3537eaa363be7973af2b423
Rutvisojitra <rutvisojitra28@gmail.com>
2025-02-07 00:16:27 +0530

    Add files via upload

---
+++ p4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    float x;
+
+    printf("enter the value of x \n");
+    scanf("%f",&x);
+    printf("x IS %f",x);
+
+    printf("%f\n",sqrt(x));
+    printf("%f\n",pow(x,2));
+    printf("%f\n",ceil(x));
+    printf("%f\n",floor(x));
+    printf(" %f",fabs(x));
+
+}

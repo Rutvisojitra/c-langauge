2e5bdbac14dba4d6d4faa03979e4f787a37acc25
Rutvisojitra <rutvisojitra28@gmail.com>
2025-02-07 00:16:11 +0530

    Add files via upload

---
+++ p3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdbool.h>
+void main()
+{
+   
+    printf("%d \n",sizeof(int));
+    printf("%d \n",sizeof(char));
+    printf("%d \n",sizeof(float));
+    printf("%d \n",sizeof(bool));
+    printf("%d \n",sizeof(double));
+    printf("%d \n",sizeof(long));
+    printf("%d \n",sizeof(short));
+
+}

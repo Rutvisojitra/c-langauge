d6f8c5c7048cb1731a94dbd2e25b61abc7fd178f
Rutvisojitra <rutvisojitra28@gmail.com>
2025-02-07 00:15:29 +0530

    Add files via upload

---
+++ b.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int j,k;
+
+    printf("enter the value of j \n");
+scanf("%d",&j);
+
+printf("enter the value of k \n");
+scanf("%d",&k);
+
+printf("%d",j==k||1>k);
+}
